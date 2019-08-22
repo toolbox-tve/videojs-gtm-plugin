b87a4b0937e0e93fc62698efa7f6a0a5b9e8a8cd
Dario Seminara <dseminara@tbxnet.com>
2019-08-22 15:58:53 -0300

    refactor main plugin module into a class

--- src/plugin.js
+++ src/plugin.js
@@ -1,186 +1,238 @@
 import videojs from 'video.js';
 import {version as VERSION} from '../package.json';
 
+const Plugin = videojs.getPlugin('plugin');
+
 // Default options for the plugin.
-const defaults = {};
+const defaults = {
+  contentTitle: '',
+  additionalData: {
+    name: 'Untitled'
+  },
+  contentLabel: 'Untitled',
+  percentiles: []
+};
 
 // Cross-compatibility for Video.js 5 and 6.
 const registerPlugin = videojs.registerPlugin || videojs.plugin;
 // const dom = videojs.dom || videojs;
 
-const createGtmDataLayer = (options) => {
-  const dataLayerVarName = 'dataLayer' + Math.floor(Math.random() * 1000);
-
-  if (options.gtmDataLayer) {
-    return () => options.gtmDataLayer;
-  }
-
-  if (!options.gtmKey) {
-    return noDataLayer;
-  }
-
-  /* global document */
-  const firstScriptTag = document.getElementsByTagName('script')[0];
-  const newScriptTag = document.createElement('script');
-  const key = options.gtmKey;
-
-  newScriptTag.async = true;
-  newScriptTag.src = 'https://www.googletagmanager.com/gtm.js?id=' + key + '&l=' + dataLayerVarName;
-  firstScriptTag.parentNode.insertBefore(newScriptTag, firstScriptTag);
-
-  const noDataLayer = {
-    push: () => {}
-  };
-
-  /* global window */
-  return () => window[dataLayerVarName] || noDataLayer;
-};
 /**
- * Function to invoke when the player is ready.
- *
- * This is a great place for your plugin to initialize itself. When this
- * function is called, the player will have its DOM and child components
- * in place.
- *
- * @function onPlayerReady
- * @param    {Player} player
- *           A Video.js player object.
- *
- * @param    {Object} [options={}]
- *           A plain object containing options for the plugin.
+ *  GTM Plugin main class
  */
-const onPlayerReady = (player, options) => {
-  const gtmDataLayer = createGtmDataLayer(options);
-  const contentLabel = options.contentLabel;
-  const additionalData = options.additionalData;
-  let pendingPercentiles = (options.percentiles || []).slice(0);
-  let lastTime = 0;
-  let viewedMilliseconds = 0;
-  let lastStartTime = null;
+class Gtm extends Plugin {
+  /**
+   * Create a gtm plugin instance.
+   *
+   * @param  {Player} player
+   *         A Video.js Player instance.
+   *
+   * @param  {Object} [options]
+   *         An optional options object.
+   *
+   *         While not a core part of the Video.js plugin architecture, a
+   *         second argument of options is a convenient way to accept inputs
+   *         from your plugin's caller.
+   */
+  constructor(player, options) {
+    super(player);
 
-  function computePlayingTime() {
-    if (lastStartTime !== null) {
-      const delta = (new Date().getTime() - lastStartTime);
+    this.gtmDataLayer = this.createGtmDataLayer(options);
 
-      viewedMilliseconds = viewedMilliseconds + delta;
+    if (options) {
+      this.contentLabel = options.contentLabel;
+      this.additionalData = options.additionalData;
+      this.pendingPercentiles = (options.percentiles || []).slice(0);
+      this.percentiles = options.percentiles;
+    } else {
+      this.contentLabel = '';
+      this.additionalData = {};
+      this.pendingPercentiles = [];
+      this.percentiles = [];
+    }
 
-      gtmDataLayer().push({
+    this.lastTime = 0;
+    this.viewedMilliseconds = 0;
+    this.lastStartTime = null;
+    this.firstTimeUpdate = null;
+
+    this.options = videojs.mergeOptions(defaults, options);
+    this.player.ready(() => {
+      this.player.addClass('vjs-concurrence-limiter');
+
+      this.player.on('play', this.onPlayerPlay.bind(this));
+      this.player.on('ended', this.onPlayerEnded.bind(this));
+      this.player.on('pause', this.onPlayerPause.bind(this));
+      this.player.on('timeupdate', this.onTimeUpdate.bind(this));
+      player.addClass('vjs-tbx-gtm');
+    });
+  }
+
+  /**
+   * Deregisters all event handlers (method called from video.js).
+   */
+  dispose() {
+    // remove al event hanlders
+    this.player.off('play', this.onPlayerPlay.bind(this));
+    this.player.off('ended', this.onPlayerEnded.bind(this));
+    this.player.off('pause', this.onPlayerPause.bind(this));
+    this.player.off('timeupdate', this.onTimeUpdate.bind(this));
+
+    // Trigger the event needed on dispose
+    super.dispose();
+  }
+
+  /**
+   * Computes playing time from recorded time states
+   */
+  computePlayingTime() {
+    if (this.lastStartTime !== null) {
+      const delta = (new Date().getTime() - this.lastStartTime);
+
+      this.viewedMilliseconds = this.viewedMilliseconds + delta;
+
+      this.gtmDataLayer().push({
         event: 'videoDetail',
         eventCategory: 'video',
         eventAction: 'Totalidad minutos vistos',
-        eventLabel: contentLabel,
-        additionalData
+        eventLabel: this.contentLabel,
+        additionalData: this.additionalData
       });
     }
 
-    lastStartTime = null;
+    this.lastStartTime = null;
   }
 
-  function onPlayerPlay(e) {
-    computePlayingTime();
-    lastStartTime = new Date().getTime();
+  /**
+   * Event handler for 'play' video.js event
+   */
+  onPlayerPlay() {
+    this.computePlayingTime();
+    this.lastStartTime = new Date().getTime();
 
-    gtmDataLayer().push({
+    this.gtmDataLayer().push({
       event: 'videoDetail',
       eventCategory: 'video',
       eventAction: 'play',
-      eventLabel: contentLabel,
-      additionalData
+      eventLabel: this.contentLabel,
+      additionalData: this.additionalData
     });
   }
 
-  function onPlayerPause(e) {
-    computePlayingTime();
-    gtmDataLayer().push({
+  /**
+   * Event handler for 'pause' video.js event
+   */
+  onPlayerPause() {
+    this.computePlayingTime();
+    this.gtmDataLayer().push({
       event: 'videoDetail',
       eventCategory: 'video',
       eventAction: 'pause',
-      eventLabel: contentLabel,
-      additionalData
+      eventLabel: this.contentLabel,
+      additionalData: this.additionalData
     });
   }
 
-  function onPlayerEnded(e) {
-    computePlayingTime();
+  /**
+   * Event handler for 'ended' video.js event
+   */
+  onPlayerEnded() {
+    this.computePlayingTime();
   }
 
-  let firstTimeUpdate = null;
-  let initialized = false;
-
-  function onTimeUpdate(e) {
-    if (!initialized) {
-      if (firstTimeUpdate === null) {
-        firstTimeUpdate = Math.floor(player.currentTime());
-      } else if (firstTimeUpdate !== Math.floor(player.currentTime)) {
+  /**
+   * Event handler for 'timeupdate' video.js event
+   */
+  onTimeUpdate() {
+    if (!this.initialized) {
+      if (this.firstTimeUpdate === null) {
+        this.firstTimeUpdate = Math.floor(this.player.currentTime());
+      } else if (this.firstTimeUpdate !== Math.floor(this.player.currentTime)) {
         // reportar Inicio
-        gtmDataLayer().push({
+        this.gtmDataLayer().push({
           event: 'videoDetail',
           eventCategory: 'video',
           eventAction: 'inicio',
-          eventLabel: contentLabel,
-          additionalData
+          eventLabel: this.contentLabel,
+          additionalData: this.additionalData
         });
 
-        initialized = true;
+        this.initialized = true;
       }
     }
 
-    if (lastTime > player.currentTime()) {
-      const fraction = player.currentTime() / player.duration();
+    if (this.lastTime > this.player.currentTime()) {
+      const fraction = this.player.currentTime() / this.player.duration();
 
       // reset persentiles
-      pendingPercentiles = options.percentiles.filter(p => p >= fraction);
+      this.pendingPercentiles = this.percentiles.filter(p => p >= fraction);
     }
 
-    if (pendingPercentiles.length && initialized) {
-      const fraction = player.currentTime() / player.duration();
-      const percentil = pendingPercentiles[0];
+    if (this.pendingPercentiles.length && this.initialized) {
+      const fraction = this.player.currentTime() / this.player.duration();
+      const percentil = this.pendingPercentiles[0];
 
       if (fraction >= percentil) {
-        gtmDataLayer().push({
+        this.gtmDataLayer().push({
           event: 'videoDetail',
           eventCategory: 'video',
           eventAction: 'consumo-porcentual',
           eventLabel: (percentil * 100) + '%',
-          additionalData
+          additionalData: this.additionalData
         });
 
-        pendingPercentiles.shift();
+        this.pendingPercentiles.shift();
       }
     }
 
-    lastTime = player.currentTime();
+    this.lastTime = this.player.currentTime();
   }
 
-  player.on('play', onPlayerPlay);
-  player.on('ended', onPlayerEnded);
-  player.on('pause', onPlayerPause);
-  player.on('timeupdate', onTimeUpdate);
-  player.addClass('vjs-tbx-gtm');
-};
+  /**
+   * Creates a GTM Data layer based on plugin options
+   *
+   * @param  {Object} options
+   *         Options object for the plugin
+   *
+   * @return {Function} gtmDayaLayer
+   *         Function returning instance of gtmDataLayer
+   */
+  createGtmDataLayer(options) {
+    const dataLayerVarName = 'dataLayer' + Math.floor(Math.random() * 1000);
+    const noDataLayer = {
+      push: () => {}
+    };
 
-/**
- * A video.js plugin.
- *
- * In the plugin function, the value of `this` is a video.js `Player`
- * instance. You cannot rely on the player being in a "ready" state here,
- * depending on how the plugin is invoked. This may or may not be important
- * to you; if not, remove the wait for "ready"!
- *
- * @function gtm
- * @param    {Object} [options={}]
- *           An object of options left to the plugin author to define.
- */
-const gtm = function(options) {
-  this.ready(() => {
-    onPlayerReady(this, videojs.mergeOptions(defaults, options));
-  });
-};
+    if (!options) {
+      return noDataLayer;
+    }
+
+    if (options.gtmDataLayer) {
+      return () => options.gtmDataLayer;
+    }
+
+    if (!options.gtmKey) {
+      return noDataLayer;
+    }
+
+    /* global document */
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    const newScriptTag = document.createElement('script');
+    const key = options.gtmKey;
+
+    newScriptTag.async = true;
+    newScriptTag.src = 'https://www.googletagmanager.com/gtm.js?id=' + key + '&l=' + dataLayerVarName;
+    firstScriptTag.parentNode.insertBefore(newScriptTag, firstScriptTag);
+
+    /* global window */
+    return () => window[dataLayerVarName] || noDataLayer;
+  }
+}
 
 // Register the plugin with video.js.
-registerPlugin('gtm', gtm);
+registerPlugin('gtm', Gtm);
 
 // Include the version number.
-gtm.VERSION = VERSION;
+Gtm.VERSION = VERSION;
 
-export default gtm;
+export default Gtm;
